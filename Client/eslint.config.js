5aa76797eeee7a5f4f42510d3a7e8e98062abe80
bigboyTaysh <pvtryk.w@gmail.com>
2025-10-11 12:29:20 +0200

    chore: package update and eslint gitignore fix

--- Client/eslint.config.js
+++ Client/eslint.config.js
@@ -13,7 +13,7 @@ import tseslint from "typescript-eslint";
 // File path setup
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(__dirname, "../.gitignore");
 
 const baseConfig = tseslint.config({
   extends: [eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic],
